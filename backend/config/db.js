7f94689d37f060eb072de7fb9c77fefb2814acaf
Radhika Pitla <radhikapitla7@gmail.com>
2026-07-21 23:38:46 +0530

    Enhance db.js for TiDB connection with SSL
    Updated database connection configuration to include port and SSL settings, and changed connection message to reflect TiDB.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,26 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
 });
+
 db.connect((err) => {
   if (err) {
     console.log("Database Connection Failed");
     console.log(err);
   } else {
-    console.log("✅ MySQL Connected");
+    console.log("✅ TiDB Connected");
   }
 });
+
 module.exports = db;
